4c75df39dc51b202395d12c842031b34eeec982b
mecukuryurt <mecukuryurt@gmail.com>
2025-06-25 19:54:55 +0300

    unoptimized print

--- badapple.py
+++ badapple.py
@@ -1,5 +1,8 @@
 import cv2
 import curses
+import time
+import os
+import numpy
 
 from curses import wrapper
 
@@ -18,7 +21,7 @@ def main(stdscr):
         stdscr.refresh()
         stdscr.getkey()
 
-wrapper(main)
+# wrapper(main)
 
 # Function to extract frames
 def FrameCapture(path):
@@ -33,17 +36,25 @@ def FrameCapture(path):
     success = 1
 
     while success:
-
+        width  = vidObj.get(3)  # float `width`
+        height = vidObj.get(4)  # float `height`
+        ## print(width, height)
         # vidObj object calls read
         # function extract frames
         success, image = vidObj.read()
 
         # Saves the frames with frame-count
-        for i in image:
-            for j in i:
-                if j.any():
-                    print(j,end="")
-            print("ok", count)
+        
+        if True:
+            for i in range(90):
+                for j in range(120):
+                    if not  (image[i*4][j*4] == numpy.array([0,0,0])).all():
+                        print("#",end="")
+                    else:
+                        print(" ", end="")
+                print()
+            os.system("cls")
+        print(type(image[0][0]))
 
         count += 1
 
@@ -52,4 +63,4 @@ def FrameCapture(path):
 if __name__ == '__main__':
     pass
     # Calling the function
-    # FrameCapture("Bad Apple.mp4")
+    FrameCapture("Bad Apple.mp4")
